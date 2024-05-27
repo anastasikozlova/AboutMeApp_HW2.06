9084a16449efd2cf52712bbfc45d4160e324984a
Anastasia Kozlova <kozlova.sia@yandex.ru>
2024-05-27 15:23:20 +0200

    Initial Commit

---
+++ AboutMeApp_HW2.06/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMeApp_HW2.06
+//
+//  Created by Анастасия Козлова on 27.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
