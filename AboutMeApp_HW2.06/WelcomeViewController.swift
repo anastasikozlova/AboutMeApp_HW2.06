0996f866a2dd5c145a8195280f874028c8daa693
Anastasia Kozlova <kozlova.sia@yandex.ru>
2024-05-29 14:35:35 +0200

    Setup UI

---
+++ AboutMeApp_HW2.06/WelcomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeViewController.swift
+//  AboutMeApp_HW2.06
+//
+//  Created by Анастасия Козлова on 27.05.2024.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
