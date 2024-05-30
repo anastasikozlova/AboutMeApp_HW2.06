71ba19c05b9958f8c9c2960a55cfa8f2f1e03b2d
Anastasia Kozlova <kozlova.sia@yandex.ru>
2024-05-30 21:08:56 +0200

    Setup app's logic

--- AboutMeApp_HW2.06/LoginViewController.swift
+++ AboutMeApp_HW2.06/LoginViewController.swift
@@ -7,13 +7,62 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
 
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard userNameTextField.text == userName, passwordTextField.text == password else {
+            let alert = UIAlertController(title: "Invalid login or password", message: "Please enter correct login and password", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+                self.userNameTextField.text = ""
+                self.passwordTextField.text = ""
+            }
+            alert.addAction(okAction)
+            present(alert, animated: true)
+            return false
+        }
+        return true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let welcomeVC = segue.destination as? WelcomeViewController
+        welcomeVC?.welcomeUser = userNameTextField.text
+    }
+    
+    @IBAction func forgotUserNameButtonTapped(_ sender: UIButton) {
+        showAlert(withTitle: "Oops!", andMessage: "Your name is \(userName)")
+    }
+    
+    @IBAction func forgotPasswordButtonTapped(_ sender: UIButton) {
+        showAlert(withTitle: "Oops!", andMessage: "Your password is \(password)")
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        _ = segue.source as? WelcomeViewController
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    private let userName = "User"
+    private let password = "1234"
+    
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
+
 
